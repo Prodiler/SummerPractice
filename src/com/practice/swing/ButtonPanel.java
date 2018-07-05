476ec7ed41bc5a61cf6f2c04ad73c94c4aca7703
daria <intgrf@yandex.ru>
2018-07-05 22:23:13 +0300

    add updated files

--- src/com/practice/swing/ButtonPanel.java
+++ src/com/practice/swing/ButtonPanel.java
@@ -3,9 +3,9 @@ package com.practice.swing;
 import javax.swing.*;
 
  public class ButtonPanel extends JPanel {
-    private final JButton startButton = createButton("Start", 40, 50);
-    private final JButton stepButton = createButton("Step", 200, 50);
-    private final JButton fileButton = createButton("Upload from file", 125, 15);
+    private final JButton startButton = createButton("Start", 10, 50);
+    private final JButton stepButton = createButton("Step", 170, 50);
+    private final JButton fileButton = createButton("Upload from file", 90, 15);
 
     public ButtonPanel() {
         super(null);
@@ -21,4 +21,5 @@ import javax.swing.*;
         createdButton.setBorderPainted(true);
         return createdButton;
     }
+
 }
